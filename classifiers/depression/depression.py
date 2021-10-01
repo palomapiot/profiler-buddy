1bb81fcd183606a38995bc13c4c2d27a1904bfb5
Paloma Piot <paloma.piot@udc.es>
2021-10-01 14:05:59 +0200

    add license header

--- classifiers/depression/depression.py
+++ classifiers/depression/depression.py
@@ -1,3 +1,19 @@
+"""
+    Copyright 2020-2021 Paloma Piot Pérez-Abadín
+	
+	This file is part of profiler-buddy.
+    profiler-buddy is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+    profiler-buddy is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+    You should have received a copy of the GNU General Public License
+    along with profiler-buddy.  If not, see <https://www.gnu.org/licenses/>.
+"""
+
 import numpy as np
 import os 
 import collections
